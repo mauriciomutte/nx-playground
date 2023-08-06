e093c8729acda96ff0673ba0627111d19fdc81da
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2023-08-06 20:41:57 -0300

    fix(app): adjust app files

---
+++ apps/app/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@nx-cicd-testes/ui';
+
+export function Index() {
+  return (
+    <div>
+      <h1>Welcome to app!</h1>
+      <Button name="teste" />
+    </div>
+  );
+}
+
+export default Index;
